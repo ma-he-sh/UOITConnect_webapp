8606c139baf5ee0119abc9d45032cd0bd17130e0
Mahesh Ranaweera <mi6softlab@gmail.com>
2016-09-26 12:29:50 -0400

    code update

---
+++ assets/php/validate.php
@@ -0,0 +1,15 @@
+<?php
+#Validate the inputs 
+
+function valid_stud_ID($stud_id){
+	if(preg_match('/^\d{9}$/'), $stud_id){
+		#check the student id for a numerical input
+		return TRUE;
+	}
+	return FALSE;
+}
+
+function valid_stud_name
+  if
+?>
+
