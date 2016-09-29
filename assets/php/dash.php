ceb8ab03937b33b763dc08ec63702c60607d3d80
Mahesh Ranaweera <mi6softlab@gmail.com>
2016-09-29 09:53:43 -0400

    code update

---
+++ assets/php/dash.php
@@ -0,0 +1,25 @@
+<?php
+include 'db.php';
+
+
+
+
+
+
+
+
+/*
+*Calender page action
+*/
+
+
+
+
+
+/*
+* Retireve user info for the interface
+*/
+ $userID   = $_SESSION['user_ID'];
+ $userName = $_SESSION['user_name'];
+
+?>
