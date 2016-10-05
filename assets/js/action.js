37fc74cc0a773bd64c55e1cd2318c426c9eba177
Mahesh Ranaweera <mi6softlab@gmail.com>
2016-10-05 06:31:23 -0400

    embeded the calender

--- assets/js/action.js
+++ assets/js/action.js
@@ -1,15 +1,15 @@
 //Calender page actions
 function display_new_card() {
-    //alert("Helo");
+  //alert("Helo");
 
-    var insertCARD;
-    var insertDIV = document.getElementById('insertDIV').;
+  var insertCARD;
+  var insertDIV = document.getElementById('insertDIV');
 
-    insertDIV.innerHTML = '<div class="dash-sch-insert-card transition"> <div class = "dash-flex-input-container"><input type = "text" class ="dash-sch-input-txt" placeholder = "| Enter Course ID"><button type = "submit" class = "dash-sch-submit-btn" onclick = "display_new_card();"> <i class = "fa fa-plus-circle fa-2x" aria-hidden = "true"></i></button></div><div class = "dash-sch-header-wrapper"><green-txt>Course submitted successfully</green-txt></div></div>';;
+  /*insertCARD = '<div class="dash-sch-insert-card transition"> <div class = "dash-flex-input-container"><input type = "text" class ="dash-sch-input-txt" placeholder = "| Enter Course ID"><button type = "submit" class = "dash-sch-submit-btn" onclick = "display_new_card();"> <i class = "fa fa-plus-circle fa-2x" aria-hidden = "true"></i></button></div><div class = "dash-sch-header-wrapper"><green-txt>Course submitted successfully</green-txt></div></div>';*/
 
-    /*insertDIV.innerHTML = '<div class="dash-sch-insert-card transition"> < div class = "dash-flex-input-container" > < input type = "text" class ="dash-sch-input-txt" placeholder = "| Enter Course ID"><button type = "submit" class = "dash-sch-submit-btn" onclick = "display_new_card();"> <i class = "fa fa-plus-circle fa-2x" aria-hidden = "true"></i></button></div><div class = "dash-sch-header-wrapper"><green-txt>Course submitted successfully</green-txt></div></div>';*/
+  insertDIV.innerHTML = 'Hello';
 
-    //insertDIV.appendChild(insertCARD);
-
+  var creNode = document.createElement(insertCARD);
+  insertDIV.appendChild(creNode);
 
 }
